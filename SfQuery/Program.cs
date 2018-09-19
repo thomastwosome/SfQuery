d79eaed50f25034fd258c2cc844d9536a77c10aa
Laura Thomas <lthomas@edcoe.org>
2018-09-19 16:57:10 -0700

    Added Dev vs Prod

--- SfQuery/Program.cs
+++ SfQuery/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace SfQuery
 {
-    class Program
+    public class Program
     {
+        private static SalesforceClient CreateClient()
+        {
+            var myEnvironment = ConfigurationManager.AppSettings["myEnvironment"];
+            var client = new SalesforceClient();
+
+            if (myEnvironment == "Dev")
+            {
+                client.Username = ConfigurationManager.AppSettings["username"];
+                client.Password = ConfigurationManager.AppSettings["password"];
+                client.Token = ConfigurationManager.AppSettings["token"];
+                client.ClientId = ConfigurationManager.AppSettings["clientId"];
+                client.ClientSecret = ConfigurationManager.AppSettings["clientSecret"];            
+            }
+            else if (myEnvironment == "Prod")
+            {
+                client.Username = ConfigurationManager.AppSettings["devUsername"];
+                client.Password = ConfigurationManager.AppSettings["devPassword"];
+                client.Token = ConfigurationManager.AppSettings["devToken"];
+                client.ClientId = ConfigurationManager.AppSettings["devClientId"];
+                client.ClientSecret = ConfigurationManager.AppSettings["devClientSecret"];           
+            }
+
+            return client;
+        }
+
         static void Main(string[] args)
         {
+            var client = CreateClient();
+
+            if (args.Length > 0)
+            {
+                client.Login();
+                Console.WriteLine(client.Query(args[0]));
+            }
+            else
+            {
+                client.Login();
+                Console.WriteLine(client.Describe("Account"));
+                Console.WriteLine(client.Describe("Contact"));
+                Console.WriteLine(client.QueryEndpoints());
+                Console.WriteLine(client.Query("SELECT Name from Contact"));
+            }
+            Console.ReadLine();
         }
     }
 }
